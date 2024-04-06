fa4ff341abe1fc182e281b341da36e6894d1a1b1
Madhuri Velaga <mvelaga1999@outlook.com>
2024-04-06 08:15:40 +0100

    update day 3 and added day 4 translation.py

--- day3/sentiment-analysis.py
+++ day3/sentiment-analysis.py
@@ -1,5 +1,7 @@
 from transformers import pipeline
 
 classifier = pipeline('sentiment-analysis')
-response = classifier('I dont appreciate that tone')
+response = classifier(
+    ["I've been waiting for a HuggingFace course my whole life.", "I hate this so much!"]
+)
 print(response)
